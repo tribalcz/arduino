99614cc05657e16eae11e008a5bc7a7532a6ad5f
tribalcz <tribal.cz@gmail.com>
2019-03-09 15:06:50 +0100

    example file

--- RELAY_LIB/example/example.ino
+++ RELAY_LIB/example/example.ino
@@ -4,12 +4,12 @@ RELAY R1(8); //Relay initialization
 
 void setup(void) {
     Serial.begin(9600); //Serial communication settings. The required -> library reports the current status of the relay on the sérial monitor.
-    R1.zapni(); //Relay closing
-    R1.vypni(); //Releasing the relay
+    R1.on(); //Relay closing
+    R1.off(); //Releasing the relay
 
 }
 
 void loop(void) {
-    R1.prepni();//switching the relay without having to enter a switch or unlock command
+    R1.switch();//switching the relay without having to enter a switch or unlock command
     delay(1000);
 }
